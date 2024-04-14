ec2fec703e236447837dfc2457edf6c643133226
[Arjun Gupta] <[guptaarjun519@gmail.com]>
2024-04-14 16:36:32 +0530

    initialize server, connect db and routes

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;;
+
+app.listen(PORT, () => {
     console.log(`App is running on port number 3000`);
 });
+
+const todoRoutes = require("./routes/todos");
+app.use("api/v1", todoRoutes);
+
+const dbConnect = require("./config/database");
+dbConnect();
+
+app.use("/", (req, res) => {
+    res.send("Welcome to the Todo App");
+})

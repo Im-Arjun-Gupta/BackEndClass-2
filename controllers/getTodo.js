c9ba539b7e8525b689aefe39495e500db3f2d506
[Arjun Gupta] <[guptaarjun519@gmail.com]>
2024-04-16 23:20:42 +0530

    created getTodoById api

--- controllers/getTodo.js
+++ controllers/getTodo.js
@@ -17,3 +17,22 @@ exports.getTodo = async (req, res) => {
         });
     }
 }
+
+exports.getTodoById = async (req, res) => {
+    try {
+        const id = req.params.id
+        const response = await Todo.findById({_id:id});
+        res.status(200).json({
+            success: true,
+            data: response,
+            message: "Entry retrieved successfully"
+        })
+    } catch(err) {
+        console.log("error", err);
+        res.status(500).json({
+            success: false,
+            data: "Internal server error",
+            message: err.message
+        })
+    }
+}
